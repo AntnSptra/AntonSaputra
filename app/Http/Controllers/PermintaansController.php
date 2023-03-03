14b0ff5bdd2f528c48860e986a5f9c5eccf7f8db
AntnSptra <antnsptra5@gmail.com>
2023-03-03 10:23:37 +0700

    first

---
+++ app/Http/Controllers/PermintaansController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Permintaans;
+use Illuminate\Http\Request;
+
+class PermintaansController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Permintaans  $permintaans
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Permintaans $permintaans)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Permintaans  $permintaans
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Permintaans $permintaans)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Permintaans  $permintaans
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Permintaans $permintaans)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Permintaans  $permintaans
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Permintaans $permintaans)
+    {
+        //
+    }
+}
---
+++ database/factories/PermintaansFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Permintaans::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
